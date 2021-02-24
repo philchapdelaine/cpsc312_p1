deae360d00f1bcd9d805b1a6bdd1f9b03278834a
Philippe Chapdelaine <37817723+philchapdelaine@users.noreply.github.com>
2021-02-24 15:26:49 -0800

    opens in a window

--- life.hs
+++ life.hs
@@ -5,15 +5,19 @@ import Life_backend
 
 -- everything below is gloss stuff
 
-main :: IO ()
-main = do
-    animate FullScreen white makePicture 
+window :: Display
+window = InWindow "Game of Pobable Life" (800, 800) (1, 1)
+
+background :: Color
+background = white
 
 makePicture :: Float -> Picture
 makePicture n = rectangleSolid n n
 
+main :: IO ()
+main = do
+    animate window background makePicture
+
+
 -- render new board after every second, gets passed the time in seconds since program start
 -- renderBoard :: Float -> Picture
-
-
-
