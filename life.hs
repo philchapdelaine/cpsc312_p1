86c3ca46f1fd3d02d55c4fe0a3812a1cb01eb8ef
Philippe Chapdelaine <37817723+philchapdelaine@users.noreply.github.com>
2021-02-25 15:26:10 -0800

    inital state + grid displaying

--- life.hs
+++ life.hs
@@ -1,5 +1,8 @@
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Animate
+import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Interface.IO.Game
+import Graphics.Gloss.Interface.Pure.Game
 
 import Life_backend
 
@@ -19,12 +22,59 @@ background = white
 makePicture :: Float -> Picture
 makePicture n = rectangleSolid n n
 
+-- Description of a state of the world
+data World = Game
+    { aliveCells :: [(Float,Float)],
+    time :: Float,
+    fps :: Int	}
+
+-- Initial state of the board
+initialWorld = Game
+    {
+    aliveCells = [(1,1),(2,2)],
+    time = 0.0,
+    fps = 20	}
+
 main :: IO ()
-main = do
-    animate
-    window -- window
-    background -- background color
-    makePicture -- starting state of the board
+main = play
+  window
+  background
+  20
+  initialWorld
+  drawingFunc
+  inputHandler
+  updateFunc
+
+drawingFunc :: World -> Picture
+drawingFunc world = Pictures $ grid ++ aliveCellsPictures
+   where aliveCellsPictures = [drawCell (a,b) | (a,b) <- aliveCells world]
+
+-- Defines a grid space
+w = (fromIntegral width)
+h = (fromIntegral height)
+
+-- x, y cells
+x = 50
+y = 50
+
+-- defines the grid and the size of each cell
+grid = verticalLines ++ horizontalLines ++ [rectangleWire w h]
+    where verticalLines = foldr (\a -> \b -> vLine a:b) [] [0..x]
+          vLine a = color  (greyN 0.5)  (line [ (w/x*a-w/2, -h/2), (w/x*a-w/2, h-h/2) ])
+          horizontalLines = foldr (\a -> \b -> hLine a:b) [] [0..y]
+          hLine a = color  (greyN 0.5)  (line [ (-w/2, h/y*a-h/2), (w-w/2, h/y*a-h/2) ])
+
+drawCell (x0,y0) =  translate (x0*w/x -w/2 +  w/x/2) (-y0*h/y +h/2 -h/y/2) square
+
+-- Filled in cells look like this
+square = rectangleSolid (w/x) (h/y)
+
+inputHandler :: Event -> World -> World
+-- handleKeys (EventKey (SpecialKey KeyUp) Down _ _) world = world { fps = changeFps (fps world) 5 }
+inputHandler _ w = w
+
+updateFunc :: Float -> World -> World
+updateFunc _ world = world
 
 
 -- render new board after every second, gets passed the time in seconds since program start
